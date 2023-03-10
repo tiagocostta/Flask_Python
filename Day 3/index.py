ef0104b9817a3060ce7fc0da411cd775900d89d1
GetulioLT <88693489+GetulioLT@users.noreply.github.com>
2023-03-10 15:44:26 -0300

    terceiro day

--- Day 3/index.py
+++ Day 3/index.py
@@ -3,10 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route("/index.html")
 @app.route("/")
 def index():
     return render_template(
          'index.html'
     )
 
-app.run()
+@app.route("/new_contatos.html")
+def contatos():
+    return render_template(
+        'new_contatos.html'
+    )
+
+app.run(debug=True)
