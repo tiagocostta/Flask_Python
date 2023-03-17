7fca1b447d5dd9b3387568e7a6ce9f3794cce309
GetulioLT <88693489+GetulioLT@users.noreply.github.com>
2023-03-17 10:41:16 -0300

    quarto dia

--- Day 4/index.py
+++ Day 4/index.py
@@ -1,3 +1,4 @@
+from lista import usuarios
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,7 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template(
          'index.html',
-         teste = False
+         usuarios = usuarios
     )
 
 app.run(debug=True)
