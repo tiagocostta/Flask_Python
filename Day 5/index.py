4d23d96eab7bb9de60f422f3dd7237ffb507d668
GetulioLT <88693489+GetulioLT@users.noreply.github.com>
2023-03-17 15:01:22 -0300

    quinto dia

--- Day 5/index.py
+++ Day 5/index.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+from lista import lista_contatos
 
 app = Flask(__name__)
 
@@ -13,7 +14,8 @@ def index():
 @app.route("/contatos.html")
 def contatos():
     return render_template(
-        'contatos.html'
+        'contatos.html',
+        lista_contatos = lista_contatos
     )
 
 @app.route("/new_contatos.html")
@@ -27,5 +29,23 @@ def sobre():
     return render_template(
         'sobre.html '
     )
+
+@app.route("/salvar_contato", methods=['POST'])
+def salvar_contato():
+    if request.form.to_dict()['tag'] == "família":
+        img = "group"
+    elif request.form.to_dict()['tag'] == "trabalho":
+        img = "business_center"
+    else:
+        img = "people"
+
+    lista_contatos.append({
+        "nome": request.form.to_dict()['nome'],
+        "email": request.form.to_dict()['email'],
+        "celular": request.form.to_dict()['celular'],
+        "tag":request.form.to_dict()['tag'],
+        'link': f'{img}'
+    })
+    return redirect('/contatos.html')
 
 app.run(debug=True)
--- Day 5/lista.py
+++ Day 5/lista.py
@@ -1,4 +1,4 @@
-usuarios = [
+lista_contatos = [
   {
     "nome": "João da Silva",
     "email": "joao.silva@gmail.com",
@@ -31,6 +31,41 @@ usuarios = [
     "nome": "Lucas Rodrigues",
     "email": "lucas.rodrigues@gmail.com",
     "celular": "(41) 92345-6789",
+    "tag": "trabalho",
+    "link": "business_center"
+  },
+  {
+    "nome": "Camila Almeida",
+    "email": "camila.almeida@gmail.com",
+    "celular": "(11) 95678-4321",
+    "tag": "família",
+    "link": "group"
+  },
+  {
+    "nome": "Rafael Barbosa",
+    "email": "rafael.barbosa@outlook.com",
+    "celular": "(21) 93456-7890",
+    "tag": "trabalho",
+    "link": "business_center"
+  },
+  {
+    "nome": "Gabriel Souza",
+    "email": "gabriel.souza@yahoo.com.br",
+    "celular": "(31) 94567-8901",
+    "tag": "amigo",
+    "link": "people"
+  },
+  {
+    "nome": "Bianca Oliveira",
+    "email": "bianca.oliveira@hotmail.com",
+    "celular": "(81) 90123-4567",
+    "tag": "família",
+    "link": "group"
+  },
+  {
+    "nome": "Marcelo Santos",
+    "email": "marcelo.santos@gmail.com",
+    "celular": "(41) 98765-4321",
     "tag": "trabalho",
     "link": "business_center"
   }
